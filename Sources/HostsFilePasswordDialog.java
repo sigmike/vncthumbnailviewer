ba69db852fcf8b3c26eaf2a1e2c0eeb2db71584d
czechtech <czechtech@0893bd0a-2c44-0410-b257-bb32fb7553b4>
2008-04-02 00:03:19 +0000

    GUI Tweaks
    git-svn-id: http://vncthumbnailviewer.googlecode.com/svn/trunk/VncThumbnailViewer@39 0893bd0a-2c44-0410-b257-bb32fb7553b4

--- Sources/HostsFilePasswordDialog.java
+++ Sources/HostsFilePasswordDialog.java
@@ -27,10 +27,16 @@ import java.awt.*;import java.awt.event.*;public class HostsFilePasswordDialo
     add(p);
     // To protect login info...
     if(isSaving) {
-      Label e = new Label("To protect login information\n enter a password to encrypt\n this file with.  (To leave file\n unencrypted, click NONE)", Label.CENTER);
+      Label e1 = new Label("To protect login information", Label.CENTER);
+      Label e2 = new Label("enter a password to encrypt", Label.CENTER);
+      Label e3 = new Label("this file with.  (To leave file", Label.CENTER);
+      Label e4 = new Label("unencrypted, click NONE)", Label.CENTER);
       c.gridwidth = GridBagConstraints.REMAINDER;
-      gridbag.setConstraints(e, c);
-      add(e);
+      gridbag.setConstraints(e1, c);
+      gridbag.setConstraints(e2, c);
+      gridbag.setConstraints(e3, c);
+      gridbag.setConstraints(e4, c);
+      add(e1); add(e2); add(e3); add(e4);
     }
     // [__________________]
     passField = new TextField(20);
@@ -42,6 +48,8 @@ import java.awt.*;import java.awt.event.*;public class HostsFilePasswordDialo
     if(isSaving) {
       cancel = new Button("None");
       cancel.addActionListener(this);
+      c.anchor = GridBagConstraints.CENTER;
+      c.fill = GridBagConstraints.NONE;
       c.gridwidth = 1;
       gridbag.setConstraints(cancel, c);
       add(cancel);
@@ -49,6 +57,8 @@ import java.awt.*;import java.awt.event.*;public class HostsFilePasswordDialo
     // ( OK )
     ok = new Button("OK");
     ok.addActionListener(this);
+    c.anchor = GridBagConstraints.CENTER;
+    c.fill = GridBagConstraints.NONE;
     c.gridwidth = GridBagConstraints.REMAINDER;
     gridbag.setConstraints(ok, c);
     add(ok);
